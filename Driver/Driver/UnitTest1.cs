c3c9d64c1b6d0b6488eb0b8dcc47963cb1f2235a
alexbrunoyo <alexbrunoyo@hotmail.com>
2022-09-23 21:53:20 -0400

    First commit

---
+++ Driver/Driver/UnitTest1.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Chrome;
+
+namespace Driver
+{
+    public class Tests
+    {
+        public IWebDriver driver;
+        IWebElement searchBox, videoLink;
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.youtube.com");
+           
+            searchBox = driver.FindElement(By.Name("search_query"));
+            searchBox.SendKeys("not on the sidelines street fighter 6");
+            searchBox.Submit();
+
+            videoLink = driver.FindElement(By.XPath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer[1]/div[3]/ytd-video-renderer[1]/div[1]/ytd-thumbnail/a"));
+            videoLink.Click();
+            driver.FindElement(By.XPath("//button[@aria-label=\'Play\']")).Click();
+        }
+    }
+}
